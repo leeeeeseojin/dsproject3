ec613b3d3fdc7606685b4b7af3c2881fc4835241
leeeeeseojin <kkyuuu00@gmail.com>
2025-12-05 19:11:47 +0900

    manager 1

--- Manager.cpp
+++ Manager.cpp
@@ -1,86 +1,339 @@
 #include "Manager.h"
 #include "GraphMethod.h"
-#include <iostream>
 #include <fstream>
-#include <vector>
+#include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 
-Manager::Manager()	
-{
-	graph = nullptr;	
-	fout.open("log.txt", ios::app);
-	load = 0;	//Anything is not loaded
+// constructor - initialize manager
+Manager::Manager() {
+  graph = nullptr;
+  fout.open("log.txt", ios::app);
+  load = 0; // Anything is not loaded
 }
 
-Manager::~Manager()
-{
-	if(load)	//if graph is loaded, delete graph
-		delete graph;	
-	if(fout.is_open())	//if fout is opened, close file
-		fout.close();	//close log.txt File
+// destructor - free memory and close file
+Manager::~Manager() {
+  if (load) // if graph is loaded, delete graph
+    delete graph;
+  if (fout.is_open()) // if fout is opened, close file
+    fout.close();     // close log.txt File
 }
 
-void Manager::run(const char* command_txt){
-	ifstream fin;	//Command File Input File Stream
-	fin.open(command_txt, ios_base::in);//File open with read mode
-		
-	if(!fin) { //If command File cannot be read, Print error
-		fout<<"command file open error"<<endl;
-		return;	//Return
-	}
-	
-	fin.close();
-	return;
+// read commands from file and execute
+void Manager::run(const char *command_txt) {
+  ifstream fin;                        // Command File Input File Stream
+  fin.open(command_txt, ios_base::in); // File open with read mode
+
+  if (!fin) { // If command File cannot be read, Print error
+    fout << "command file open error" << endl;
+    return; // Return
+  }
+
+  string command;
+
+  // read commands line by line
+  // call appropriate function
+  while (fin >> command) {
+    if (command == "LOAD") {
+      string filename;
+      fin >> filename;
+      LOAD(filename.c_str());
+    }
+
+    else if (command == "PRINT") {
+      PRINT();
+    }
+
+    else if (command == "BFS") {
+      char option;
+      int vertex;
+      fin >> option >> vertex;
+      mBFS(option, vertex);
+    }
+
+    else if (command == "DFS") {
+      char option;
+      int vertex;
+      fin >> option >> vertex;
+      mDFS(option, vertex);
+    }
+
+    else if (command == "KRUSKAL") {
+      mKRUSKAL();
+    }
+
+    else if (command == "DIJKSTRA") {
+      char option;
+      int vertex;
+      fin >> option >> vertex;
+      mDIJKSTRA(option, vertex);
+    }
+
+    else if (command == "BELLMANFORD") {
+      char option;
+      int s_vertex, e_vertex;
+      fin >> option >> s_vertex >> e_vertex;
+      mBELLMANFORD(option, s_vertex, e_vertex);
+    }
+
+    else if (command == "FLOYD") {
+      char option;
+      fin >> option;
+      mFLOYD(option);
+    }
+
+    else if (command == "CENTRALITY") {
+      mCentrality();
+    }
+
+    else if (command == "EXIT") {
+      fout << "========EXIT========" << endl;
+      fout << "Success" << endl;
+      fout << "=======================" << endl;
+      break;
+    }
+  }
+
+  fin.close();
+  return;
 }
 
-bool Manager::LOAD(const char* filename)
-{
-	
+// load graph data from file
+// read graph type, size, edge information
+// create ListGraph or MatrixGraph based on type
+bool Manager::LOAD(const char *filename) {
+  ifstream fin;
+  fin.open(filename);
+
+  // if file cannot be opened, print error code 100
+  if (!fin) {
+    printErrorCode(100);
+    return false;
+  }
+
+  // if graph already exists, delete old graph
+  if (load) {
+    delete graph;
+    graph = nullptr;
+    load = 0;
+  }
+
+  char type;
+  int size;
+
+  fin >> type; // read graph type - L or M
+  fin >> size; // read graph size - number of vertices
+
+  // create graph based on type
+  if (type == 'L') {
+    // create ListGraph for adjacency list
+    graph = new ListGraph(true, size);
+
+    int vertex;
+    string line;
+
+    getline(fin, line); // skip remaining characters in size line
+
+    // read adjacency list data
+    // vertex number in one line -> edge info in next line
+    while (getline(fin, line)) {
+      // if line is empty, skip
+      if (line.empty()) {
+        continue;
+      }
+
+      // check if line contains only a vertex number
+      stringstream ss(line);
+      int firstNum;
+      ss >> firstNum;
+
+      // if only one number, it is a vertex index
+      if (ss.eof() || ss.peek() == '\n' || ss.peek() == EOF) {
+        vertex = firstNum;
+      } else {
+        // if two numbers, it is destination and weight
+        int dest = firstNum;
+        int weight;
+        ss >> weight;
+
+        // insert edge from vertex to dest with weight
+        graph->insertEdge(vertex, dest, weight);
+      }
+    }
+  } else if (type == 'M') {
+    // create MatrixGraph for adjacency matrix
+    graph = new MatrixGraph(true, size);
+
+    // read adjacency matrix data
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        int weight;
+        fin >> weight;
+
+        // if weight is non-zero, insert edge
+        if (weight != 0) {
+          graph->insertEdge(i, j, weight);
+        }
+      }
+    }
+  } else {
+    // invalid type
+    fin.close();
+    printErrorCode(100);
+    return false;
+  }
+
+  fin.close();
+  load = 1;
+
+  fout << "========LOAD========" << endl;
+  fout << "Success" << endl;
+  fout << "=======================" << endl;
+
+  return true;
 }
 
-bool Manager::PRINT()	
-{
+// print graph information
+// if graph is loaded, print adjacency list or matrix
+bool Manager::PRINT() {
+  // if graph is not loaded, print error code 200
+  if (!load || !graph) {
+    printErrorCode(200);
+    return false;
+  }
 
+  fout << "========PRINT========" << endl;
+  graph->printGraph(&fout);
+  fout << "=======================" << endl;
+
+  return true;
 }
 
-bool Manager::mBFS(char option, int vertex)	
-{
-	
+// functions that check graph state and forward the call to algorithm
+// check if graph exists and call BFS algorithm
+bool Manager::mBFS(char option, int vertex) {
+  // if graph is not loaded or vertex is invalid, print error code 300
+  if (!load || !graph || vertex < 0 || vertex >= graph->getSize()) {
+    printErrorCode(300);
+    return false;
+  }
+
+  // call BFS algorithm
+  if (!BFS(graph, option, vertex)) {
+    printErrorCode(300);
+    return false;
+  }
+
+  return true;
 }
 
-bool Manager::mDFS(char option, int vertex)	
-{
-	
+// check if graph exists and call DFS algorithm
+bool Manager::mDFS(char option, int vertex) {
+  // if graph is not loaded or vertex is invalid, print error code 400
+  if (!load || !graph || vertex < 0 || vertex >= graph->getSize()) {
+    printErrorCode(400);
+    return false;
+  }
+
+  // call DFS algorithm
+  if (!DFS(graph, option, vertex)) {
+    printErrorCode(400);
+    return false;
+  }
+
+  return true;
 }
 
-bool Manager::mDIJKSTRA(char option, int vertex)	
-{
-	
+// check if graph exists and call Dijkstra algorithm
+bool Manager::mDIJKSTRA(char option, int vertex) {
+  // if graph is not loaded or vertex is invalid, print error code 600
+  if (!load || !graph || vertex < 0 || vertex >= graph->getSize()) {
+    printErrorCode(600);
+    return false;
+  }
+
+  // call Dijkstra algorithm
+  if (!Dijkstra(graph, option, vertex)) {
+    printErrorCode(600);
+    return false;
+  }
+
+  return true;
 }
 
-bool Manager::mKRUSKAL()
-{
- 	
+// check if graph exists and call Kruskal algorithm
+bool Manager::mKRUSKAL() {
+  // if graph is not loaded, print error code 500
+  if (!load || !graph) {
+    printErrorCode(500);
+    return false;
+  }
+
+  // call Kruskal algorithm
+  if (!Kruskal(graph)) {
+    printErrorCode(500);
+    return false;
+  }
+
+  return true;
 }
 
-bool Manager::mBELLMANFORD(char option, int s_vertex, int e_vertex) 
-{
-	
+// check if graph exists and call Bellman-Ford algorithm
+bool Manager::mBELLMANFORD(char option, int s_vertex, int e_vertex) {
+  // if graph is not loaded or vertices are invalid, print error code 700
+  if (!load || !graph || s_vertex < 0 || s_vertex >= graph->getSize() ||
+      e_vertex < 0 || e_vertex >= graph->getSize()) {
+    printErrorCode(700);
+    return false;
+  }
+
+  // call Bellman-Ford algorithm
+  if (!Bellmanford(graph, option, s_vertex, e_vertex)) {
+    printErrorCode(700);
+    return false;
+  }
+
+  return true;
 }
 
-bool Manager::mFLOYD(char option)
-{
-	
+// check if graph exists and call Floyd algorithm
+bool Manager::mFLOYD(char option) {
+  // if graph is not loaded, print error code 800
+  if (!load || !graph) {
+    printErrorCode(800);
+    return false;
+  }
+
+  // call Floyd algorithm
+  if (!FLOYD(graph, option)) {
+    printErrorCode(800);
+    return false;
+  }
+
+  return true;
 }
 
+// check if graph exists and call Centrality algorithm
 bool Manager::mCentrality() {
-	
+  // if graph is not loaded, print error code 900
+  if (!load || !graph) {
+    printErrorCode(900);
+    return false;
+  }
+
+  // call Centrality algorithm
+  if (!Centrality(graph)) {
+    printErrorCode(900);
+    return false;
+  }
+
+  return true;
 }
 
-void Manager::printErrorCode(int n)
-{
-	fout<<"========ERROR======="<<endl;
-	fout<<n<<endl;
-	fout<<"===================="<<endl << endl;
+void Manager::printErrorCode(int n) {
+  fout << "========ERROR=======" << endl;
+  fout << n << endl;
+  fout << "====================" << endl << endl;
 }
-
